c5882844cb0be5412fb1c5fabc0a3898dcbdcc11
Alejandro-Chen <3408611970@qq.com>
2026-05-19 01:48:14 +0800

    Initial commit - mech engineer portfolio

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/mech-engineer-portfolio',
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/mech-engineer-portfolio/',
-  trailingSlash: true,
   distDir: 'out',
 };
 
---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+title Mech Engineer Portfolio
+echo Starting dev server...
+npm run dev
+pause
